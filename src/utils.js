ca3d556408b5f7db5bdbe52740bdbe6c4500238b
Atria94 <atriaclase@gmail.com>
2022-11-21 10:14:02 +0100

    improve test

--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,22 @@
 export function createRandomGroups(users, personsInGroup) {
-    const usersCopy = [...users] // clone
-    const totalGroups = Math.floor(users.length / personsInGroup)
-    const groups = []
-    for (let i = 0; i < totalGroups; i++) {
-      const group = []
-      for (let j = 0; j < personsInGroup; j++) {
-        const randomIndex = Math.floor(Math.random() * usersCopy.length)
-        const randomUser = usersCopy[randomIndex]
-        group.push(randomUser)
-        usersCopy.splice(randomIndex, 1)
+  const usersCopy = [...users]  //clone
+  const totalGroups = Math.floor(users.length / personsInGroup)
+  const groups=[]
+  for(let i=0; i<totalGroups; i++){
+      let group = []
+      for(let j=0; j<personsInGroup; j++){
+          const randomIndex = Math.floor(Math.random() * usersCopy.length)
+          const randomUser = usersCopy[randomIndex]
+          group.push(randomUser)
+          usersCopy.splice(randomIndex, 1)
       }
       groups.push(group)
-    }
-    return {
-      inGroups: groups,
-      noGroups: usersCopy,
-    }
   }
+  return{
+      inGroups: groups,
+      noGroups: usersCopy
+  }
+}
   
   
   /* 
